2de96dc830913625ed98370a0c9a5d38be698cf2
Lee Simpson <harveysimpsondata@gmail.com>
2023-08-11 21:47:32 -0400

    08-11-23

--- optimization_tests.py
+++ optimization_tests.py
@@ -2,7 +2,7 @@
 import time
 import os
 import datetime
-import json
+from concurrent.futures import ThreadPoolExecutor
 
 # Third-party libraries
 import pandas as pd
@@ -31,6 +31,7 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
+MAX_WORKERS = 3  # adjust this based on your system's capabilities
 
 @contextmanager
 def session_scope(engine):
@@ -41,7 +42,6 @@ def session_scope(engine):
     finally:
         connection.close()
 
-# Check database connection using psycopg2
 sslmode = 'require'
 conn_str = f"host={DB_HOST} port={DB_PORT} dbname={DB_NAME} user={DB_USERNAME} password={DB_PASSWORD} sslmode={sslmode}"
 
@@ -54,7 +54,6 @@ try:
 finally:
     cur.close()
     conn.close()
-
 
 connection_url = URL.create(
     drivername="postgresql+pg8000",
@@ -77,28 +76,7 @@ publish_table = Table(
     Column("pubber", String)
 )
 
-
-def table_exists(engine, table_name):
-    return engine.dialect.has_table(engine.connect(), table_name)
-
-
-# Create inspector for the engine
-inspector = inspect(engine)
-
-# Check if the table "publishes" exists in the database
-if "publishes" not in inspector.get_table_names():
-
-    # Create the table if it doesn't exist
-    meta.create_all(engine)
-
-    # Create the hypertable
-    with psycopg2.connect(conn_str) as conn:
-        with conn.cursor() as cur:
-            try:
-                cur.execute(f"SELECT create_hypertable('{publish_table.name}', 'create_at', migrate_data => True);")
-            except Exception as e:
-                print(f"Error creating hyper table: {e}")
-
+# ... [rest of your table and database setup code]
 
 url = "https://origintrail.api.subscan.io/api/scan/evm/token/holders"
 headers = {
@@ -112,13 +90,10 @@ data = {
 }
 
 response = requests.post(url, headers=headers, json=data).json()
-
 df = pd.DataFrame(response['data']['list'])
 pubber_list = df['holder'].tolist()
 
-postgres_data = []
-
-for pubber in pubber_list:
+def fetch_pubber_data(pubber):
     url = "https://origintrail.api.subscan.io/api/scan/evm/erc20/transfer"
     headers = {
         "Content-Type": "application/json",
@@ -130,7 +105,7 @@ for pubber in pubber_list:
     }
 
     response = requests.post(url, headers=headers, json=data).json()
-    print(response['data']['list'])
+
     df = (pd.DataFrame(response['data']['list'])
           .query('symbol == "TRAC"')
           .assign(pubber=pubber, create_at=lambda x: pd.to_datetime(
@@ -139,11 +114,17 @@ for pubber in pubber_list:
           .drop(columns=['contract', 'decimals', 'name', 'from_display', 'to_display', 'token_id', 'to', 'from'])
           .astype({'hash': str, 'symbol': str, 'pubber': str, 'create_at': 'datetime64[ns]'}))
 
-    postgres_data.extend(df.to_dict(orient='records'))
-    print(f"Publisher's Transactions Added! -> Address: {pubber}")
+    return df.to_dict(orient='records'), pubber
+
+# Use ThreadPoolExecutor to fetch data in parallel
+postgres_data = []
+with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+    for data, pub in executor.map(fetch_pubber_data, pubber_list):
+        postgres_data.extend(data)
+        print(f"Publisher's Transactions Added! -> Address: {pub}")
+
 
 # Upload data to postgres (batch)
-# create insert statement
 insert_statement = postgresql.insert(publish_table).values(postgres_data)
 
 # if the primary key already exists, update the record
