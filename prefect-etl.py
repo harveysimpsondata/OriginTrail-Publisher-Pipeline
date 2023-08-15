b8478ea6de6252992510b82e00ffd84103a7c7bd
Lee Simpson <harveysimpsondata@gmail.com>
2023-08-14 22:24:35 -0400

    08-14-23

--- prefect-etl.py
+++ prefect-etl.py
@@ -2,6 +2,7 @@
 import time
 import os
 import datetime
+from datetime import timedelta
 from concurrent.futures import ThreadPoolExecutor
 
 # Third-party libraries
@@ -19,10 +20,78 @@ from sqlalchemy.engine import URL
 from contextlib import contextmanager
 
 from prefect import task, flow
+from prefect.tasks import task_input_hash
 
-@task(log_prints=True, retries=3, retry_delay=datetime.timedelta(seconds=10))
-def ingest_data(API_KEY, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME):
+@task(log_prints=True, retries=3, cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1))
+def extract_data(pubber_address_url, transactions_url, API_KEY):
+
+
     MAX_WORKERS = 3  # adjust this based on your system's capabilities
+    url = "https://origintrail.api.subscan.io/api/scan/evm/token/holders"
+    headers = {
+        "Content-Type": "application/json",
+        "X-API-Key": API_KEY
+    }
+    data = {
+        "contract": "0x5cac41237127f94c2d21dae0b14bfefa99880630",
+        "row": 100,
+        "page": 0
+    }
+
+    response = requests.post(url, headers=headers, json=data).json()
+    df = pd.DataFrame(response['data']['list'])
+    pubber_list = df['holder'].tolist()
+
+    def fetch_pubber_transactions(pubber):
+        url = "https://origintrail.api.subscan.io/api/scan/evm/erc20/transfer"
+        headers = {
+            "Content-Type": "application/json",
+            "X-API-Key": API_KEY
+        }
+        data = {
+            "address": pubber,
+            "row": 40
+        }
+
+        response = requests.post(url, headers=headers, json=data).json()
+
+        df = (pd.DataFrame(response['data']['list']))
+
+        return df
+
+    #     df = (pd.DataFrame(response['data']['list'])
+    #           .query('symbol == "TRAC"')
+    #           .query('to == "0x61bb5f3db740a9cb3451049c5166f319a18927eb"')
+    #           .assign(pubber=pubber, create_at=lambda x: pd.to_datetime(
+    #         x['create_at'].apply(lambda y: datetime.datetime.utcfromtimestamp(y).isoformat())),
+    #                   value=lambda x: x['value'].astype(float) / 1e18)
+    #           .drop(columns=['contract', 'decimals', 'name', 'from_display', 'to_display', 'token_id', 'to', 'from'])
+    #           .astype({'hash': str, 'symbol': str, 'pubber': str, 'create_at': 'datetime64[ns]'}))
+    #
+    #     return df.to_dict(orient='records'), pubber
+    #
+    # # Use ThreadPoolExecutor to fetch data in parallel
+    # postgres_data = []
+    # with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+    #     for data, pub in executor.map(fetch_pubber_transactions, pubber_list):
+    #         postgres_data.extend(data)
+    #         print(f"Publisher's Transactions Added! -> Address: {pub}")
+    #
+    # return postgres_data
+
+@task(log_prints=True, retries=3)
+def transform_data(df, pubber):
+    df = (df.query('symbol == "TRAC"')
+            .query('to == "0x61bb5f3db740a9cb3451049c5166f319a18927eb"')
+            .assign(pubber=pubber, create_at=lambda x: pd.to_datetime(x['create_at'].apply(lambda y: datetime.datetime.utcfromtimestamp(y).isoformat())),
+                    value=lambda x: x['value'].astype(float) / 1e18)
+            .drop(columns=['contract', 'decimals', 'name', 'from_display', 'to_display', 'token_id', 'to', 'from'])
+            .astype({'hash': str, 'symbol': str, 'pubber': str, 'create_at': 'datetime64[ns]'}))
+
+
+
+@task(log_prints=True, retries=3)
+def load_data(DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME):
 
     @contextmanager
     def session_scope(engine):
@@ -72,52 +141,6 @@ def ingest_data(API_KEY, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME):
         Column("pubber", String)
     )
 
-    url = "https://origintrail.api.subscan.io/api/scan/evm/token/holders"
-    headers = {
-        "Content-Type": "application/json",
-        "X-API-Key": API_KEY
-    }
-    data = {
-        "contract": "0x5cac41237127f94c2d21dae0b14bfefa99880630",
-        "row": 100,
-        "page": 0
-    }
-
-    response = requests.post(url, headers=headers, json=data).json()
-    df = pd.DataFrame(response['data']['list'])
-    pubber_list = df['holder'].tolist()
-
-    def fetch_pubber_data(pubber):
-        url = "https://origintrail.api.subscan.io/api/scan/evm/erc20/transfer"
-        headers = {
-            "Content-Type": "application/json",
-            "X-API-Key": API_KEY
-        }
-        data = {
-            "address": pubber,
-            "row": 40
-        }
-
-        response = requests.post(url, headers=headers, json=data).json()
-
-        df = (pd.DataFrame(response['data']['list'])
-              .query('symbol == "TRAC"')
-              .query('to == "0x61bb5f3db740a9cb3451049c5166f319a18927eb"')
-              .assign(pubber=pubber, create_at=lambda x: pd.to_datetime(
-            x['create_at'].apply(lambda y: datetime.datetime.utcfromtimestamp(y).isoformat())),
-                      value=lambda x: x['value'].astype(float) / 1e18)
-              .drop(columns=['contract', 'decimals', 'name', 'from_display', 'to_display', 'token_id', 'to', 'from'])
-              .astype({'hash': str, 'symbol': str, 'pubber': str, 'create_at': 'datetime64[ns]'}))
-
-        return df.to_dict(orient='records'), pubber
-
-    # Use ThreadPoolExecutor to fetch data in parallel
-    postgres_data = []
-    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-        for data, pub in executor.map(fetch_pubber_data, pubber_list):
-            postgres_data.extend(data)
-            print(f"Publisher's Transactions Added! -> Address: {pub}")
-
     # Upload data to postgres (batch)
     insert_statement = postgresql.insert(publish_table).values(postgres_data)
 
@@ -149,7 +172,12 @@ def main_flow():
     DB_PORT = os.getenv("DB_PORT")
     DB_NAME = os.getenv("DB_NAME")
 
-    ingest_data(API_KEY, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
+    pubber_address_url="https://origintrail.api.subscan.io/api/scan/evm/token/holders"
+    transactions_url = "https://origintrail.api.subscan.io/api/scan/evm/erc20/transfer"
+
+    raw_data = extract_data(pubber_address_url, transactions_url, API_KEY)
+    transformed_data = transform_data(raw_data)
+    load_data(API_KEY, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
 
 if __name__ == "__main__":
     main_flow()
